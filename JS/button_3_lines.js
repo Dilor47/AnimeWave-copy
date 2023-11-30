2f485744c6c5397580d8b8ee4611e6848f096d7b
Dima <dimasedun9@gmail.com>
2023-11-30 17:16:53 +0200

    Finishing the appearence on many buttons

--- JS/button_3_lines.js
+++ JS/button_3_lines.js
@@ -1,7 +1,12 @@
 const dropDown_btn = document.querySelector('.js_dropDown');
 const dropDown_menu = document.querySelector('.dropDown_content');
+const button = document.querySelector('#js_change_btn');
 
 dropDown_btn.addEventListener('click', () => {
-  
+
+  button.classList.toggle('fa-xmark');
+  button.classList.toggle('fa-bars');
+
+
   dropDown_menu.classList.toggle('show');
 })
