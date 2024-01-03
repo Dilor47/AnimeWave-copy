7c0e5881b61f8db51cca168996298f88833b77d2
Dima <dimasedun9@gmail.com>
2024-01-03 19:32:45 +0200

    almost finished carousel

---
+++ JS/carousel.js
@@ -0,0 +1,50 @@
+import {anime_description} from '../data/info_to_carousel.js'
+
+const carousel = document.querySelector('.main-carousel');
+
+let carousel_html = '';
+
+anime_description.forEach((element) => {
+  carousel_html += `
+  <div class="carousel-cell" style="background: linear-gradient(to right, rgb(0, 0, 0), ${element.background}, rgb(0, 0, 0));" >
+    <div class="descr">
+      <div class="all_info_anime">
+        <h1>
+          ${element.title}
+        </h1>
+        <div class="short_info">
+          <p class="icon">${element.age_limit}</p>
+
+          <div class="icons">
+            ${element.is_there_sub}
+            ${element.is_there_vSub}
+          </div>
+          
+          <p class="date icon">${element.release_data}</p>
+        </div>
+
+        <p class="main_text">
+          ${element.main_desc}
+        </p>
+
+        <div class="carousel_button">
+          <i class="fa-solid fa-play fa-2xl"></i>
+          <p class="fa-2xl">PLAY NOW</p>
+        </div>
+      </div>
+    </div>
+    <img src="${element.image}">
+  </div>`
+});
+
+carousel.innerHTML = carousel_html;
+console.log('done');
+
+$(document).ready(() => {
+  $('.main-carousel').flickity({
+    cellAlign: 'center',
+    wrapAround: true,
+    autoPlay: true,
+    prevNextButtons: false
+  })
+})
---
+++ data/info_to_carousel.js
@@ -0,0 +1,92 @@
+export const anime_description = [
+  {
+    title: 'TORADORA',
+    age_limit: '+13',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'October 2, 2008',
+    main_desc: '"Toradora!" is a delightful romantic comedy anime centered around Ryuji and Taiga, high school students who team up to assist each other in their romantic pursuits. Despite their contrasting personalities, their partnership leads to unexpected developments in their own relationship...',
+    image: 'images/images_carousel/toradora.jpg',
+    background: 'rgb(255, 0, 140)'
+  },
+  {
+    title: 'BLEACH: THOUSAND...',
+    age_limit: '+13',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'October 11, 2022',
+    main_desc: ' "Bleach" follows Ichigo Kurosaki, a teenager with the ability to see spirits. His life takes a turn when he gains Soul Reaper powers and becomes responsible for protecting humans from malevolent spirits known as Hollows. The series follows Ichigo"s journey as he battles these supernatural threats, forms alliances...',
+    image: 'images/images_carousel/bleach_thous.jpg',
+    background: 'rgb(255, 0, 85)'
+  },
+  {
+    title: 'HAIKYUU!!',
+    age_limit: '+13',
+    is_there_sub: '',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'April 6, 2014',
+    main_desc: ' "Haikyuu!!" is an anime and manga series centered around high school volleyball. It follows Shoyo Hinata, an enthusiastic and short-statured player inspired by a legendary player known as the "Little Giant." Despite his height disadvantage, Hinata is determined to become a great volleyball player...',
+    image: "images/images_carousel/haikyu.jpg",
+    background: 'rgb(48, 7, 42)'
+  },
+  {
+    title: 'POKÉMON',
+    age_limit: '+6',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'April 1, 1997',
+    main_desc: ' "Pokemon" is an anime series set in a world where humans catch creatures known as Pokemon to train, battle, and compete. The story follows Ash Ketchum, a young Pokemon Trainer, on his quest to become a Pokemon Master. Alongside his loyal Pikachu, Ash travels through various regions... ',
+    image: "images/images_carousel/pokemons.jpg",
+    background: 'rgb(127, 25, 141)'
+  },
+  {
+    title: 'BLEACH',
+    age_limit: '+12',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'October 5, 2004',
+    main_desc: '"Bleach: Thousand War of Blood" is a fictional narrative within the "Bleach" universe that tells of an epic conflict uniting thousands of warriors and powerful beings. Within this war, various groups and characters with unique abilities and motivations battle...',
+    image: "images/images_carousel/bleach.jpg",
+    background: 'rgb(238, 36, 21)'
+  },
+  {
+    title: 'MY HERO ACADEMIA',
+    age_limit: '+13',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'April 3, 2016',
+    main_desc: '"My Hero Academia" is about a world where almost everyone has unique powers. Izuku Midoriya, a Quirkless boy, dreams of being a hero. When he inherits a powerful Quirk, he attends U.A. High School to learn to become a hero. The series follows his journey, friendships, and challenges in a world filled...',
+    image: "images/images_carousel/my_hero_aca.jpg",
+    background: 'rgb(7, 36, 165)'
+  },
+  {
+    title: 'ONE PIECE',
+    age_limit: '+13',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'December 24, 1997',
+    main_desc: '"One Piece" is an adventure-filled manga and anime about Monkey D. Luffy and his pirate crew"s quest to find the ultimate treasure, the One Piece, in order to become the Pirate King. Along their journey across vast oceans and diverse islands, they face formidable foes, make friends, and...',
+    image: "images/images_carousel/one_piece.jpg",
+    background: 'rgb(19, 189, 56)'
+  },
+  {
+    title: 'ROAD TO NINJA...',
+    age_limit: '+12',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'July 28, 2012',
+    main_desc: '"Road to Ninja: Naruto the Movie" is a film from the Naruto series where Naruto and Sakura are transported to an alternate reality. In this world, their personalities are reversed, and they encounter different versions of their friends and enemies... ',
+    image: "images/images_carousel/road_to_ninja.jpg",
+    background: 'rgb(20, 131, 175)'
+  },
+  {
+    title: 'TOKYO GHOUL',
+    age_limit: '15+',
+    is_there_sub: '<i class="fa-solid fa-closed-captioning fa-2xl icon"></i>',
+    is_there_vSub: '<i class="fa-solid fa-microphone fa-xl icon"></i>',
+    release_data: 'June 16, 2015',
+    main_desc: '"Tokyo Ghoul" is a dark fantasy manga and anime series set in Tokyo, where ghouls—creatures that look like humans but need to consume human flesh to survive—exist. Kaneki, an ordinary college student, becomes a half-ghoul after a chance encounter, gaining ghoul abilities while struggling to maintain his humanity. The story delves...',
+    image: "images/images_carousel/tokyo_ghoul.jpg",
+    background: 'rgb(209, 4, 65)'
+  },
+]
