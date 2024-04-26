00b3a2797470483b371e424e90aa4d6d847274f2
Dima <dimasedun9@gmail.com>
2024-04-26 09:29:38 +0300

    Save a normal state

---
+++ JS/continue_watch/dlt_logic_ContWatch.js
@@ -0,0 +1,10 @@
+import stopPoint_data from '../../data/current_watching_anime.js';
+/*
+const trigger = document.querySelector('.dlt_sign_cont');
+
+console.log(trigger);
+
+trigger.addEventListener('click', (e) => {
+  console.log('hihih');
+})
+*/
