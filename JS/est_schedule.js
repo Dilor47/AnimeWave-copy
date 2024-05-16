07ba815b41cae27683c3a9202f2ac42beafd39b7
Dima <dimasedun9@gmail.com>
2024-05-16 21:29:58 +0300

    trying to make carousel with the lib but it goes nto this smooth... (saved before make my own carousel)

--- JS/est_schedule.js
+++ JS/est_schedule.js
@@ -1,4 +1,3 @@
-
 let intervalId;
 
 const dates = dayjs();
@@ -14,3 +13,33 @@ intervalId = setInterval(() => {
 function cleaning_interval() {
   clearInterval(intervalId);
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  const flickity_viewport = document.querySelector('.carousel.second .flickity-viewport');
+  //const pageDotsContainer = document.querySelector('.carousel.second .flickity-page-dots');
+  if (flickity_viewport) {
+      console.log(flickity_viewport);
+      flickity_viewport.classList.add('second');
+  }
+});
+/*
+$(document).ready(() => {
+  $('.carousel.second').flickity({
+    cellAlign: 'left',
+    contain: true,
+    groupCells: 2
+  })
+})
+*/
+
+
+const flkty = new Flickity('.carousel.second', {
+  cellAlign: 'left',
+  contain: true,
+  groupCells: 2,
+  initialIndex: 3,
+  freeScroll: false,
+  /*prevNextButtons: false,*/
+});
+
+flkty.option('cellAlign', 'left');
